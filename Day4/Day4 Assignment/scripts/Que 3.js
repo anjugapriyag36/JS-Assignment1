05268da8aea851bc6301fd3ccb53e0f5c89e9f64
anjugapriyag36 <61075737+anjugapriyag36@users.noreply.github.com>
2020-07-22 13:36:54 +0530

    Que 3.js

---
+++ Day4/Day4 Assignment/scripts/Que 3.js
@@ -0,0 +1,4 @@
+var shoppingList = ['Brinjal', 'Tomato', 'Potato', 'Pumpkin'];
+console.log(shoppingList);
+var shoppingBasket = shoppingList.concat('Curry leaves', 'Mint leaves', 'Coriander leaves');
+console.log(shoppingBasket);
