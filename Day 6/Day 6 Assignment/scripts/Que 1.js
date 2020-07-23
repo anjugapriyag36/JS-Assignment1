f7c64eae764c2d75e604a71585632ebfc4780bc7
anjugapriyag36 <61075737+anjugapriyag36@users.noreply.github.com>
2020-07-23 20:55:54 +0530

    Que 1.js

---
+++ Day 6/Day 6 Assignment/scripts/Que 1.js
@@ -0,0 +1,5 @@
+setInterval(
+    function() {
+        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        document.body.style.backgroundColor = "#" + randomColor;
+    }, 5000);
